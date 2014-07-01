81351c54b1eff4314e690e3a37f86cf8b5f7570d
Melissa Loudon <melissa.loudon@gmail.com>
2014-07-01 15:54:40 -0400

    updating R db query for domain change

--- src/R/db_source_mobile_user_tables.R
+++ src/R/db_source_mobile_user_tables.R
@@ -20,14 +20,15 @@ query <- sprintf("with a as
                  from visit 
                  order by visit.user_id, time_start) 
                  select visit.visit_id, users.user_id, cases.case_id, time_start, time_end, form_duration, 
-                 a.total_forms, b.time_since_previous, visit.home_visit, users.domain 
-                 from visit, users, case_visit, cases, a,b 
+                 a.total_forms, b.time_since_previous, visit.home_visit, domain.name as domain
+                 from visit, users, case_visit, cases, a,b, domain
                  where visit.user_id = users.id 
                  and case_visit.case_id = cases.id 
                  and case_visit.visit_id = visit.visit_id 
                  and a.visit_id = visit.visit_id 
                  and b.visit_id = visit.visit_id 
-                 and users.domain in (%s) 
+                 and users.domain_id = domain.id 
+                 and domain.name in (%s) 
                  order by visit.user_id, time_start", domain_list)
 
 rs <- dbSendQuery(con,query)

6823b5daea5c094f1a85dc30d670044a8e456de3
Melissa Loudon <melissa.loudon@gmail.com>
2014-07-07 14:19:14 -0400

    mengji's R script updated

--- src/R/csv_sources.R
+++ src/R/csv_sources.R
@@ -1,16 +1,6 @@
 get_interaction_table_from_csv <- function (filename) {
 
-v <- read.csv(filename, header = TRUE)
-
-# update timedate format (importing data from csv automatically makes it into factor variable in R)
-v$time_start <- as.POSIXct(as.character(levels(v$time_start)), format = "%Y-%m-%d %H:%M:%S")[v$time_start]
-v$time_end <- as.POSIXct(as.character(levels(v$time_end)), format = "%Y-%m-%d %H:%M:%S")[v$time_end]
-v$visit_date <- as.Date(v$time_start)
-v$month.index <- as.yearmon(v$visit_date) # obtaining year and month from Date
-v$time_since_previous <- as.numeric(v$time_since_previous) # convert factor to numeric vectors
-related_cases_index_child <- which(duplicated(v$time_start)) # this returns all child cases
-v$related <- c("No")
-v$related[related_cases_index_child] <- "Yes" # this flags all multiple-case visits
+v <- read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
 
 return (v)
 }

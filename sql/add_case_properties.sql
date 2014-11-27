559d08d2d45654e12dcdf6b12ca251cbf82b1a65
Melissa Loudon <melissa.loudon@gmail.com>
2014-11-26 21:55:12 -0500

    migration to add case properties

---
+++ sql/add_case_properties.sql
@@ -0,0 +1,9 @@
+﻿select attributes->'impact_case_properties' from domain where name like 'jharkhand-mch'
+
+alter table incoming_form add column case_properties hstore;
+alter table case_event add column case_properties hstore;
+
+select form_id, case_id, case_properties 
+from incoming_form where domain like 'jharkhand-mch' and record_type like 'case_event' 
+order by id desc
+limit 11071

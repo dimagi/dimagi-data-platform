0a3720574b1e493a80ea7432c032f0d53913aec4
Melissa Loudon <melissa.loudon@gmail.com>
2014-11-28 11:48:40 -0500

    migration

--- sql/add_case_properties.sql
+++ sql/add_case_properties.sql
@@ -7,3 +7,7 @@ select form_id, case_id, case_properties
 from incoming_form where domain like 'jharkhand-mch' and record_type like 'case_event' 
 order by id desc
 limit 11071
+
+select * from case_event 
+where form_id in (select id from form where domain_id = (select id from domain where name like 'jharkhand-mch'))
+and case_properties is not null
